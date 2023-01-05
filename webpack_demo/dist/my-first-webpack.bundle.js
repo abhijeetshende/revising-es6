b81044d38cb032270f8968b37b73ac4b4606c89b
Abhijeet Shende <GS-3467@GSLAB.COM>
2023-01-05 18:15:44 +0530

    webapack import export

--- webpack_demo/dist/my-first-webpack.bundle.js
+++ webpack_demo/dist/my-first-webpack.bundle.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./webpack_demo/index.js":
 /*!*******************************!*\
   !*** ./webpack_demo/index.js ***!
   \*******************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("var count = 1234567890;\nvar str = \"The quick brown fox jumps over the lazy dog \".concat(count, \" times.\");\nvar str2 = \" MULTILINE The sun was setting over the horizon, casting\\n\\na warm glow over the landscape. The birds were singing \\ntheir evening songs and the crickets were chirping in the grass. It was a peaceful end to a busy day. As I stood there, taking in the beauty of the scene,\\n I couldn't help but feel grateful for the simple pleasures in life.\\\"\\n\\n\\n\\n\";\nprintToScreen(str);\nprintToScreen(str2);\n\n//# sourceURL=webpack://learning/./webpack_demo/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _utils_variables_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils/variables.js */ \"./webpack_demo/utils/variables.js\");\n\nconsole.log('hello index');\n(0,_utils_variables_js__WEBPACK_IMPORTED_MODULE_0__.printSecret)();\n\n//# sourceURL=webpack://learning/./webpack_demo/index.js?");
+
+/***/ }),
+
+/***/ "./webpack_demo/utils/variables.js":
+/*!*****************************************!*\
+  !*** ./webpack_demo/utils/variables.js ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Secret_Key\": () => (/* binding */ Secret_Key),\n/* harmony export */   \"printSecret\": () => (/* binding */ printSecret)\n/* harmony export */ });\nvar Secret_Key = \"secret\";\nfunction printSecret() {\n  console.log(Secret_Key);\n}\n\n\n//# sourceURL=webpack://learning/./webpack_demo/utils/variables.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./webpack_demo/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./webpack_demo/index.js");
 /******/ 	
 /******/ })()
 ;
---
+++ webpack_demo/utils/variables.js
@@ -0,0 +1,7 @@
+let Secret_Key = "secret";
+function printSecret() {
+  console.log(Secret_Key);
+}
+
+
+export{Secret_Key,printSecret}
