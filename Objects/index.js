aabee01372cba33517bdd0cf5b8599b517a0d859
Abhijeet Shende <GS-3467@GSLAB.COM>
2023-01-06 14:31:50 +0530

    prototypal inheritance in objects

--- Objects/index.js
+++ Objects/index.js
@@ -28,3 +28,24 @@ const person = {
  // as objects are point to different memory locations
  //hence not equal
  console.log(person1 === person2)
+
+
+ // if create obj with no inheritence
+
+ var person_without_inheritance = Object.create(null);
+ person_without_inheritance.name="person_without_inheritance";
+ console.log(person_without_inheritance);
+
+
+ 
+ // if create obj with  inheritence
+
+ var person_with_inheritance = Object.create(person1);
+ person_with_inheritance.name="person_with_inheritance";
+ /**
+  * {name: 'person_with_inheritance'}
+    name : "person_with_inheritance"
+   [[Prototype]]:  Object age : 30 name: "abhijeet"[[Prototype]]: Object  
+  * 
+  */
+ console.log(person_with_inheritance);
